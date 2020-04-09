2038f0e50cff53846d2e00058a4ee4b670af2f6c
Tracy Yu <tracyyu93@g.ucla.edu>
2020-04-08 18:34:06 -0700

    Add text animations

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -5,19 +5,19 @@
     </div>
     <div class="homepage-image_container">
       <div class="homepage-image_wrapper">
-        <div class="text-upper-left-wrapper">
-          <span>Taking the</span>
-        </div>
-        <div class="text-upper-right-wrapper">
+        <div class="text-upper-right-wrapper" v-scrollanimation>
           <span>Best of Both Worlds</span>
+        </div>
+        <div class="text-upper-left-wrapper" v-scrollanimation>
+          <span>Taking the</span>
         </div>
         <div class="image_wrapper">
           <img src="../assets/images/Homepage.png" alt="Main Homepage">
         </div>
-        <div class="text-lower-right-wrapper">
+        <div class="text-lower-right-wrapper" v-scrollanimation>
           <span>Immerse Yourself</span>
         </div>
-        <div class="text-lower-left-wrapper">
+        <div class="text-lower-left-wrapper" v-scrollanimation>
           <span>Warp Reality</span>
         </div>
       </div>
